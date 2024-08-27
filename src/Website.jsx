924c8f46f07a2522584b0f684882dd9bb67ba982
Voß <cvoss@INTRANET.KAMPMANN.DE>
2024-08-27 17:41:33 +0200

    Erster Commit zu test für das Programmierprojekt

---
+++ src/Website.jsx
@@ -0,0 +1,16 @@
+import './Website.css';
+
+function Website(){
+    return (
+        <div className="Website">
+            <header className='Website-header'>
+                <p>
+                    Hier entsteht der Header der Website
+                </p>
+            </header>
+        </div>
+    )
+}
+
+export default Website; 
+
