c07434f69d59fb11efeab038b4ab271e6f907312
Nadine <nadine.arning@hs-osnabrueck.de>
2024-08-30 10:33:40 +0200

    HTML Test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Website from './Website';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import Website from './Website';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Website />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Website />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Unsere Simplex Seite</title>
+
+</head>
+<body>
+
+
+
+<article> 
+<h1 style="font-family:verdana;">Willkommen auf unserer Simplex Seite</h1>
+
+<p
+style="font-family:Calibri;">Mit diesem Werkzeug können Lineare Optimierungsprobleme (LP) online gelöst werden. Das Werkzeug wendet den Simplexalgorithmus an. Es stehen zwei Ein­gabe­möglichkeiten zur Verfügung und das Ergebnis kann unterschiedlich detailliert angezeigt werden.</p>
+</article>
+
+
+</body>
+</html> 
